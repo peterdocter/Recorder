a9a4259634073ec34b4fae867eb598cd06486d60
zhangjiahao <zhangjiahao@letv.com>
2015-06-10 00:25:57 +0800

    use proxy of presenter
    Signed-off-by: zhangjiahao <zhangjiahao@letv.com>

--- src/com/letv/MVPRecorder/presenter/BasePresenter.java
+++ src/com/letv/MVPRecorder/presenter/BasePresenter.java
@@ -3,10 +3,14 @@ package com.letv.MVPRecorder.presenter;
 import android.os.Binder;
 import android.os.IBinder;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * Created by zhangjiahao on 2015/6/7.
  */
-public abstract class BasePresenter<T,K> {
+public abstract class BasePresenter<T, K> {
 
     private Binder mPresenterBinder;
 
@@ -14,6 +18,22 @@ public abstract class BasePresenter<T,K> {
 
     public BasePresenter() {
         mPresenterBinder = new PresenterBinder();
+    }
+
+    private InvocationHandler handler = new InvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object result = null;
+            synchronized (this) {
+                result = method.invoke(BasePresenter.this, args);
+            }
+            return result;
+        }
+    };
+
+    private T getPresenterProxy() {
+        T instance = (T) Proxy.newProxyInstance(getClass().getClassLoader(), getClass().getInterfaces(), handler);
+        return instance;
     }
 
     public IBinder getBinder() {
@@ -24,9 +44,8 @@ public abstract class BasePresenter<T,K> {
 
         public T newPresenter(K view) {
             BasePresenter.this.view = view;
-            return onCreatePresenter();
+            return getPresenterProxy();
         }
     }
 
-    protected abstract T onCreatePresenter();
 }
